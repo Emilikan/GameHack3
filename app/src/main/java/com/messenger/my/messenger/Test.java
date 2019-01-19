7c4429df1b2ad9a3fc25b664cdd9555b03c88fa3
DanilaSuvorov <dansuvtim@gmail.com>
2019-01-20 01:51:21 +0300

    upload

--- app/src/main/java/com/messenger/my/messenger/Test.java
+++ app/src/main/java/com/messenger/my/messenger/Test.java
@@ -32,6 +32,8 @@ public class Test extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_test, container, false);
+
+
         Bundle bundle = getArguments();
         String counterOfFragment = "0";
         if (bundle != null) {
--- app/src/main/java/com/messenger/my/messenger/TestQuestions.java
+++ app/src/main/java/com/messenger/my/messenger/TestQuestions.java
@@ -1,11 +1,14 @@
 package com.messenger.my.messenger;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -16,12 +19,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.Objects;
+
+
 
 
 public class TestQuestions extends Fragment {
 
-
+    String counterOfFragment;
+    Integer counter = 1;
+    EditText answer, question;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,37 +43,77 @@ public class TestQuestions extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_test_questions, container, false);
 
-        View rootView = inflater.inflate(R.layout.fragment_test_questions, container, false);
 
-        final EditText mQu1 = rootView.findViewById(R.id.question1);
-        final EditText mQu2 = rootView.findViewById(R.id.question2);
-        final EditText mQu1Ans1 = rootView.findViewById(R.id.variant1_1);
-        final EditText mQu1Ans2 = rootView.findViewById(R.id.variant1_2);
-        final EditText mQu1Ans3 = rootView.findViewById(R.id.variant1_3);
-        final EditText mQu2Ans1 = rootView.findViewById(R.id.variant2_1);
-        final EditText mQu2Ans2 = rootView.findViewById(R.id.variant2_2);
-        final EditText mQu2Ans3 = rootView.findViewById(R.id.variant2_3);
+        Bundle bundle = getArguments();
+        counterOfFragment = "0";
+        if (bundle != null) {
+            counterOfFragment = bundle.getString("Value", "0");
+        }
+
+
 
         Button button = rootView.findViewById(R.id.upload);
         button.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onClick(View v) {
-                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
-                SharedPreferences.Editor editor = preferences.edit();
-                editor.putString("Question1", mQu1.getText().toString().trim());
-                editor.putString("Question1_1", mQu1Ans1.getText().toString().trim());
-                editor.putString("Question1_2", mQu1Ans2.getText().toString().trim());
-                editor.putString("Question1_3", mQu1Ans3.getText().toString().trim());
-                editor.putString("Question2", mQu2.getText().toString().trim());
-                editor.putString("Question2_1", mQu2Ans1.getText().toString().trim());
-                editor.putString("Question2_2", mQu2Ans2.getText().toString().trim());
-                editor.putString("Question2_3", mQu2Ans3.getText().toString().trim());
-                editor.apply();
 
-                Toast.makeText(getContext(), "Тест успешно загружен!", Toast.LENGTH_LONG ).show();
+                answer = rootView.findViewById(R.id.answer);
+                question = rootView.findViewById(R.id.question1);
+
+
+                // объявляем переменную
+                final DatabaseReference mRef = FirebaseDatabase.getInstance().getReference();
+
+// сохраняем данные
+                mRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        mRef.child("Books").child("0").child("Tests").child(counter.toString()).child("Answer").setValue(answer);
+                        mRef.child("Books").child("0").child("Tests").child(counter.toString()).child("Question").setValue(question);
+                        mRef.child("Books").child("0").child("Tests").child(counter.toString()).child("counter").setValue(counter);
+
+                    }
+                    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                        builder.setTitle("Error")
+                                .setMessage(databaseError.getMessage())
+                                .setCancelable(false)
+                                .setNegativeButton("Ок, закрыть",
+                                        new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int id) {
+                                                dialog.cancel();
+                                            }
+                                        });
+                        AlertDialog alert = builder.create();
+                        alert.show();
+                    }
+                });
+
+
+                Toast.makeText(getContext(),"Тест успешно загружен!", Toast.LENGTH_LONG).show();
                 Fragment fragment = new Profile();
+                FragmentTransaction fragmentManager = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
+                fragmentManager.replace(R.id.container2, fragment).commit();
+            }
+        });
+
+
+
+
+        Button button2 = rootView.findViewById(R.id.next_test);
+        button.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+                counter++;
+                Fragment fragment = new TestQuestions();
                 FragmentTransaction fragmentManager = Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction();
                 fragmentManager.replace(R.id.container2, fragment).commit();
             }
@@ -69,4 +122,8 @@ public class TestQuestions extends Fragment {
 
         return rootView;
     }
+
+
+
+
 }
